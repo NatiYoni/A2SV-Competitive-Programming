610fea41e6f774fbf0e137c55701d8656b330677
Natnael Yonas <148974950+NatiYoni@users.noreply.github.com>
2025-02-24 23:24:26 +0300

    Created 'Fox And Snake' by A2SV Hub

---
+++ 24-Feb-2025/Fox And Snake 282074.py
@@ -0,0 +1,28 @@
+# Problem: Fox And Snake - https://codeforces.com/problemset/problem/510/A
+
+
+def invr():
+    return map(int, input().split())
+
+
+
+def solve():
+    n, m = invr()
+    for i in range(1,n + 1):
+        if i % 2 == 1:
+            print('#' * m)
+        else:
+            if i % 4 == 0:
+                print("#" + "." * (m-1))
+            else:
+                print("." * (m-1) + "#")
+
+
+def main():
+    multi = False 
+    t = int(input()) if multi else 1
+    for _ in range(t):
+        solve()
+
+if __name__ == '__main__':
+    main()
