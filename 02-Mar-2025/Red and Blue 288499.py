ec46a57a7070d7aee89ca78eb16d64d3fd14c1d3
Natnael Yonas <148974950+NatiYoni@users.noreply.github.com>
2025-03-03 00:49:10 +0300

    Created 'Red and Blue' by A2SV Hub

---
+++ 02-Mar-2025/Red and Blue 288499.py
@@ -0,0 +1,38 @@
+# Problem: Red and Blue - https://codeforces.com/contest/1469/problem/B
+
+import sys
+import collections
+input = sys.stdin.readline
+
+def inp():
+    return int(input())
+def inlt():
+    return list(map(int, input().split()))
+
+
+def solve():
+    n = inp()
+    r = inlt()
+    m = inp()
+    b = inlt()
+
+
+    
+    for i in range(1,n): 
+        r[i] += r[i-1]
+    for i in range(1,m):
+        b[i] += b[i-1] 
+        
+    num_b , num_r = max(b) , max(r)
+    print(max(max(num_b,0) + max(num_r,0),0))        
+
+
+
+def main():
+    multi = True
+    t = int(input()) if multi else 1
+    for _ in range(t):
+        solve()
+
+if __name__ == '__main__':
+    main()
