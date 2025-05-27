02180949dc844fabe199a0bf350a6fe005c3c3a1
Natnael Yonas <yonasnatnael11@gmail.com>
2025-05-27 21:30:07 +0300

    Created 'Wrong Subtraction' by A2SV Hub

---
+++ 27-May-2025/Wrong Subtraction 360280.py
@@ -0,0 +1,44 @@
+# Problem: Wrong Subtraction - https://codeforces.com/problemset/problem/977/A?mobile=false
+
+import sys, collections, math, random, bisect
+
+input = lambda: sys.stdin.readline().strip()
+
+inp = lambda: int(input())
+inlt = lambda: list(map(int, input().split()))
+insr = lambda: list(input())
+invr = lambda: map(int, input().split())
+
+def dd(type_func=int):
+    return collections.defaultdict(type_func)
+
+def is_in_bounds(grid, i, j):
+    return 0 <= i < len(grid) and 0 <= j < len(grid[0])
+
+directions = [(-1,0),(0,1),(0,-1),(1,0),(1,1),(-1,-1),(-1,1),(1,-1)]
+
+rand_no = random.randint(1, 1000000000)
+
+def solve():
+    n,k = invr()
+
+    while k:
+        if n % 10 == 0:
+ 
+            n //= 10
+        else:
+            n -= 1
+        k -= 1
+  
+
+    print(n)
+    pass
+
+def main():
+    multi = False  # Set to True for multi-test cases
+    t = inp() if multi else 1
+    for _ in range(t):
+        solve()
+
+if __name__ == '__main__':
+    main()
