1f9904cfc47ed5026a7747674484789202270ea2
Natnael Yonas <yonasnatnael11@gmail.com>
2025-06-05 22:13:42 +0300

    Created 'Team' by A2SV Hub

---
+++ 05-Jun-2025/Team 364082.py
@@ -0,0 +1,12 @@
+# Problem: Team - https://codeforces.com/contest/231/problem/A
+
+n = int(input())
+count = 0
+
+for _ in range(n):
+    p, v, t = map(int, input().split())
+
+    if p + v + t >= 2:
+        count += 1
+
+print(count)
