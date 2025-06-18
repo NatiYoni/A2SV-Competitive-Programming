642c96f5748195215af7e881ac353bdaf7d4b2e0
Natnael Yonas <yonasnatnael11@gmail.com>
2025-06-18 22:42:23 +0300

    Created 'Skibidus and Fanum Tax (hard version)' by A2SV Hub

---
+++ 18-Jun-2025/Skibidus and Fanum Tax (hard version) 367917.py
@@ -0,0 +1,53 @@
+# Problem: Skibidus and Fanum Tax (hard version) - http://codeforces.com/problemset/problem/2065/C2
+
+import sys, collections, math, random,bisect
+
+input = lambda: sys.stdin.readline().strip()
+
+inp = lambda: int(input())
+inlt = lambda: list(map(int, input().split()))
+insr = lambda: list(input())
+invr = lambda: map(int, input().split())
+
+def dd(type_func=int):
+    return collections.defaultdict(type_func)
+
+def is_in_bounds(grid, i, j):
+    return 0 <= i < len(grid) and 0 <= j < len(grid[0])
+
+directions = [(-1,0),(0,1),(0,-1),(1,0),(1,1),(-1,-1),(-1,1),(1,-1)]
+
+rand_no = random.randint(1, 1000000000)
+
+
+def solve():
+
+    n, m = invr()
+    arr1 = inlt()
+    arr2 = inlt()
+    arr2.sort()  
+
+    p,c = arr1[0], arr2[0] - arr1[0]
+    for i in range(1,n):
+        temp = min(p,c)
+        p = math.inf
+        c = math.inf
+
+        if arr1[i] >= temp:
+            p = arr1[i]
+        
+        left = bisect.bisect_left(arr2, arr1[i] + temp)
+        if left < m:
+            c = arr2[left] - arr1[i]
+    
+    print("Yes") if min(p,c) != math.inf else print("No")
+  
+
+def main():
+    multi = True  # Set to True for multi-test cases
+    t = inp() if multi else 1
+    for _ in range(t):
+        solve()
+
+if __name__ == '__main__':
+    main()
