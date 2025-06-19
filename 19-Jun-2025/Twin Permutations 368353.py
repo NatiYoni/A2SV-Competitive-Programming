0f29118134ce09abf3eb5c5512390316a3bb3736
Natnael Yonas <yonasnatnael11@gmail.com>
2025-06-19 23:09:14 +0300

    Created 'Twin Permutations' by A2SV Hub

---
+++ 19-Jun-2025/Twin Permutations 368353.py
@@ -0,0 +1,42 @@
+# Problem: Twin Permutations - https://codeforces.com/contest/1831/problem/A
+
+import sys, collections, math, random, bisect
+
+input = lambda: sys.stdin.readline().strip()
+
+inp = lambda: int(input())
+inlt = lambda: list(map(int, input().split()))
+insr = lambda: list(input())
+invr = lambda: map(int, input().split())
+
+def dd(type_func=int):
+    return collections.defaultdict(type_func)
+
+def is_in_bounds(grid, i, j):
+    return 0 <= i < len(grid) and 0 <= j < len(grid[0])
+
+directions = [(-1,0),(0,1),(0,-1),(1,0),(1,1),(-1,-1),(-1,1),(1,-1)]
+
+rand_no = random.randint(1, 1000000000)
+
+def solve():
+    n = inp()
+    arr = inlt()
+    mx = max(arr) + 1
+    
+    ans = []
+    for i in range(n):
+        ans.append(mx - arr[i])
+
+    print(*ans) 
+    
+
+
+def main():
+    multi = True  # Set to True for multi-test cases
+    t = inp() if multi else 1
+    for _ in range(t):
+        solve()
+
+if __name__ == '__main__':
+    main()
