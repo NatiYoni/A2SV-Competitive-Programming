21dcacc0508ad075fbc4cd52a1404ed8b2a93da3
Natnael Yonas <yonasnatnael11@gmail.com>
2025-07-10 22:52:26 +0300

    Created 'Domino Piling' by A2SV Hub

---
+++ 10-Jul-2025/Domino Piling 375283.py
@@ -0,0 +1,35 @@
+# Problem: Domino Piling - https://codeforces.com/problemset/problem/50/A
+
+import sys, collections, math, random, bisect
+
+input = lambda: sys.stdin.readline().strip()
+
+inp = lambda: int(input())
+inlt = lambda: list(map(int, input().split()))
+insr = lambda: list(input())
+invr = lambda: map(int, input().split())
+
+def dd(type_func=int):
+    return collections.defaultdict(type_func)
+
+def is_in_bounds(grid, i, j):
+    return 0 <= i < len(grid) and 0 <= j < len(grid[0])
+
+directions = [(-1,0),(0,1),(0,-1),(1,0),(1,1),(-1,-1),(-1,1),(1,-1)]
+
+rand_no = random.randint(1, 1000000000)
+
+def solve():
+    n,m = invr()
+
+    print((n*m) // 2)
+    pass
+
+def main():
+    multi = False  # Set to True for multi-test cases
+    t = inp() if multi else 1
+    for _ in range(t):
+        solve()
+
+if __name__ == '__main__':
+    main()
