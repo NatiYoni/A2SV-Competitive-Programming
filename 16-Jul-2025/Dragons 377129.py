9bda4a442195f84a6da35d4f9378fd02e443510c
Natnael Yonas <yonasnatnael11@gmail.com>
2025-07-16 23:11:52 +0300

    Created 'Dragons' by A2SV Hub

---
+++ 16-Jul-2025/Dragons 377129.py
@@ -0,0 +1,48 @@
+# Problem: Dragons - https://codeforces.com/problemset/problem/230/A
+
+import sys, collections, math, random, bisect
+
+input = lambda: sys.stdin.readline().strip()
+
+inp = lambda: int(input())
+inlt = lambda: list(map(int, input().split()))
+insr = lambda: list(input())
+invr = lambda: map(int, input().split())
+
+def dd(type_func=int):
+    return collections.defaultdict(type_func)
+
+def is_in_bounds(grid, i, j):
+    return 0 <= i < len(grid) and 0 <= j < len(grid[0])
+
+directions = [(-1,0),(0,1),(0,-1),(1,0),(1,1),(-1,-1),(-1,1),(1,-1)]
+
+rand_no = random.randint(1, 1000000000)
+
+def solve():
+    s, n = invr()
+
+    arr = []
+    for _ in range(n):
+        x,y = invr()
+        arr.append((x,y))
+    arr.sort()
+
+    for x,y in arr:
+        if s > x:
+            s += y
+        else:
+            print("NO")
+            return
+    
+    print("YES")
+    pass
+
+def main():
+    multi = False  # Set to True for multi-test cases
+    t = inp() if multi else 1
+    for _ in range(t):
+        solve()
+
+if __name__ == '__main__':
+    main()
