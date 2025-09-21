08764d5aefefc223846805be13ccaafe30529abf
Natnael Yonas <yonasnatnael11@gmail.com>
2025-09-21 12:34:19 +0300

    Created 'Python Check if All the Integers in a Range Are Covered' by A2SV Hub

---
+++ 21-Sep-2025/Python Check if All the Integers in a Range Are Covered 394608.py
@@ -0,0 +1,18 @@
+# Problem: Python Check if All the Integers in a Range Are Covered - https://leetcode.com/problems/check-if-all-the-integers-in-a-range-are-covered/description/
+
+class Solution:
+    def isCovered(self, ranges: List[List[int]], left: int, right: int) -> bool:
+        arr = [0] * 52
+
+        for st, end in ranges:
+            arr[st] += 1
+            arr[end+1] -= 1
+        
+        for i in range(1, len(arr)):
+            arr[i] += arr[i-1]
+
+        for i in range(left, right + 1):
+            if arr[i] == 0:
+                return False
+        
+        return True
